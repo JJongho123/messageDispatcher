463fb0e4d3a6f38fd8db82674aea4cbcd9d07d9f
JJongho123 <112939338+JJongho123@users.noreply.github.com>
2024-11-23 20:42:55 +0900

    .
    .

--- src/main/java/org/message/MessageHandler.java
+++ src/main/java/org/message/MessageHandler.java
@@ -1,28 +1,28 @@
-package org.message;
-
-/**
- * MessageHandler 클래스는 메시지 발송 전략을 관리하며,
- * 전달받은 메시지를 처리하는 역할을 합니다.
- */
-public class MessageHandler {
-
-    private final Message messageStrategy;
-
-    /**
-     *
-     * @param messageStrategy 사용할 메시지 발송 전략
-     */
-    public MessageHandler (Message messageStrategy) {
-        this.messageStrategy = messageStrategy;
-    }
-
-    /**
-     * 메시지를 처리하고 발송하는 메서드.
-     *
-     * @param message 발송할 메시지
-     */
-    public void sendMessage(String message) {
-        messageStrategy.sendMessage(message);
-    }
-
-}
+//package org.message;
+//
+///**
+// * MessageHandler 클래스는 메시지 발송 전략을 관리하며,
+// * 전달받은 메시지를 처리하는 역할을 합니다.
+// */
+//public class MessageHandler {
+//
+//    private final Message messageStrategy;
+//
+//    /**
+//     *
+//     * @param messageStrategy 사용할 메시지 발송 전략
+//     */
+//    public MessageHandler (Message messageStrategy) {
+//        this.messageStrategy = messageStrategy;
+//    }
+//
+//    /**
+//     * 메시지를 처리하고 발송하는 메서드.
+//     *
+//     * @param message 발송할 메시지
+//     */
+//    public void sendMessage(String message) {
+//        messageStrategy.sendMessage(message);
+//    }
+//
+//}
--- src/main/java/org/message/ThreadPool.java
+++ src/main/java/org/message/ThreadPool.java
@@ -1,48 +1,48 @@
-package org.message;
-
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.Executor;
-import java.util.concurrent.LinkedTransferQueue;
-
-/**
- * ThreadPool 클래스는 사용자 지정 크기의 스레드풀을 관리하며,
- * 작업을 병렬로 처리할 수 있도록 설계되었습니다.
- */
-public class ThreadPool implements Executor{
-
-    private final BlockingQueue<Runnable> queue= new LinkedTransferQueue<>();
-
-    /**
-     * 지정된 크기의 스레드풀을 초기화하고 각 스레드에서 작업 큐를 처리합니다.
-     *
-     * @param numThreads 스레드풀의 크기
-     */
-    public ThreadPool(int numThreads) {
-
-        Thread[] threads = new Thread[numThreads];
-        for (int i = 0; i < numThreads; i++) {
-            threads[i]=new Thread(()->{
-                try {
-                    for(;;) {
-                        // 작업 큐에서 작업을 가져와 실행
-                        final Runnable task = queue.take();
-                        task.run();
-                    }
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            });
-            threads[i].start();
-        }
-    }
-
-    /**
-     * 작업 큐에 새로운 Runnable 작업을 추가합니다.
-     *
-     * @param command 실행할 Runnable 작업
-     */
-    @Override
-    public void execute(Runnable command) {
-        queue.add(command);
-    }
-}
+//package org.message;
+//
+//import java.util.concurrent.BlockingQueue;
+//import java.util.concurrent.Executor;
+//import java.util.concurrent.LinkedTransferQueue;
+//
+///**
+// * ThreadPool 클래스는 사용자 지정 크기의 스레드풀을 관리하며,
+// * 작업을 병렬로 처리할 수 있도록 설계되었습니다.
+// */
+//public class ThreadPool implements Executor{
+//
+//    private final BlockingQueue<Runnable> queue= new LinkedTransferQueue<>();
+//
+//    /**
+//     * 지정된 크기의 스레드풀을 초기화하고 각 스레드에서 작업 큐를 처리합니다.
+//     *
+//     * @param numThreads 스레드풀의 크기
+//     */
+//    public ThreadPool(int numThreads) {
+//
+//        Thread[] threads = new Thread[numThreads];
+//        for (int i = 0; i < numThreads; i++) {
+//            threads[i]=new Thread(()->{
+//                try {
+//                    for(;;) {
+//                        // 작업 큐에서 작업을 가져와 실행
+//                        final Runnable task = queue.take();
+//                        task.run();
+//                    }
+//                } catch (InterruptedException e) {
+//                    throw new RuntimeException(e);
+//                }
+//            });
+//            threads[i].start();
+//        }
+//    }
+//
+//    /**
+//     * 작업 큐에 새로운 Runnable 작업을 추가합니다.
+//     *
+//     * @param command 실행할 Runnable 작업
+//     */
+//    @Override
+//    public void execute(Runnable command) {
+//        queue.add(command);
+//    }
+//}
