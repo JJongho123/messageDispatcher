1c4876a37377c3c6fe13d57eaaa52a8a31acab64
JJongho123 <112939338+JJongho123@users.noreply.github.com>
2024-11-23 20:12:04 +0900

    javadoc update
    - javadoc update
    - README.md update

--- src/main/java/org/message/Main.java
+++ src/main/java/org/message/Main.java
@@ -1,8 +1,12 @@
 package org.message;
 
+/**
+ * 메시지 핸들러, 메시지 발송 Runnable, 그리고 스레드풀을 초기화하고 실행합니다.
+ */
 public class Main {
     public static void main(String[] args) {
 
+        // 메시지 발송 방식 설정 (기본 메시지 발송 방식 사용)
         MessageHandler msgHandler = new MessageHandler(new DefaultMessage());
 
         MessageSendingRunnable  runnable = new MessageSendingRunnable(msgHandler);
