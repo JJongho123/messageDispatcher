b22d925c56124c218d4dbedba23b70edafd716b6
JJongho123 <112939338+JJongho123@users.noreply.github.com>
2024-11-23 20:37:07 +0900

    update
    update

--- src/main/java/org/message/DefaultMessage.java
+++ src/main/java/org/message/DefaultMessage.java
@@ -1,27 +1,27 @@
-package org.message;
-
-/**
- * DefaultMessage 클래스는 Message 인터페이스의 기본 구현체입니다.
- * 메시지를 500ms 대기 후 표준 출력으로 출력합니다.
- */
-public class DefaultMessage implements Message {
-
-    /**
-     * 메시지를 발송하는 메서드.
-     *
-     * @param msg 발송할 메시지
-     */
-    @Override
-    public void sendMessage(String msg) {
-
-        try{
-            // 500ms 대기 후 메시지 출력
-            Thread.sleep(500);
-        }
-        catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
-
-        System.out.println("보낸 메시지 출력 : " + msg);
-    }
-}
+//package org.message;
+//
+///**
+// * DefaultMessage 클래스는 Message 인터페이스의 기본 구현체입니다.
+// * 메시지를 500ms 대기 후 표준 출력으로 출력합니다.
+// */
+//public class DefaultMessage implements Message {
+//
+//    /**
+//     * 메시지를 발송하는 메서드.
+//     *
+//     * @param msg 발송할 메시지
+//     */
+//    @Override
+//    public void sendMessage(String msg) {
+//
+//        try{
+//            // 500ms 대기 후 메시지 출력
+//            Thread.sleep(500);
+//        }
+//        catch (InterruptedException e) {
+//            Thread.currentThread().interrupt();
+//        }
+//
+//        System.out.println("보낸 메시지 출력 : " + msg);
+//    }
+//}
--- src/main/java/org/message/Message.java
+++ src/main/java/org/message/Message.java
@@ -1,15 +1,15 @@
-package org.message;
-
-/**
- * Message 인터페이스는 메시지 발송 방식을 정의
- */
-interface Message {
-
-    /**
-     * 메시지를 발송하는 메서드.
-     *
-     * @param msg 발송할 메시지
-     */
-    void sendMessage(String msg);
-
-}
+//package org.message;
+//
+///**
+// * Message 인터페이스는 메시지 발송 방식을 정의
+// */
+//interface Message {
+//
+//    /**
+//     * 메시지를 발송하는 메서드.
+//     *
+//     * @param msg 발송할 메시지
+//     */
+//    void sendMessage(String msg);
+//
+//}
